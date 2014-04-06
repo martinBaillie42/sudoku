353d39848340b597c01c6ba02bade1dbe39e0943
Martin Baillie <martin@martinbaillie.net>
2014-04-06 23:03:27 +0100

    probably nearly finished

--- src/sudoku/sudoku2.scala
+++ src/sudoku/sudoku2.scala
@@ -1,6 +1,3 @@
-/**
- *
- */
 package sudoku
 
 import scala.io.Source._
@@ -12,137 +9,115 @@ import javax.swing._
  */
 object sudoku2 {
 
-  def main(args: Array[String]): Unit = {
-    val fileList = formatFile(loadFile)
-    val sudokuGrid = genSudokuGrid(fileList)
+    def main(args: Array[String]): Unit = {
+        val fileList = formatFile(loadFile)
+        val sudokuGrid = genSudokuGrid(fileList)
 
-//    println(sudokuGrid foreach (x => x foreach (y => println(y)) ))
-//    println(sudokuGrid foreach (x => println(x(0))))
-    val completeSudokuGrid = solveSudoku(sudokuGrid)
+        val completeSudokuGrid = solveSudoku(sudokuGrid)
 
-//    completeSudokuGrid foreach (println(_))
-//    println(completeSudokuGrid foreach (x => println(x(0))))
-//      println(completeSudokuGrid(0) foreach (x => println(x)))
-//
-//    println(completeSudokuGrid foreach (x => x foreach (y => println(y)) ))
-    for (y <- 0 until 9) {
-      for (x <- 0 until 9) {
-        if (completeSudokuGrid(x)(y).size == 1) {
-          print(completeSudokuGrid(x)(y)(0) + " ")
-        } else {
-          print("_ ")
+        displayGrid(completeSudokuGrid)
+
+    }
+
+    def displayGrid(completeSudokuGrid: Array[Array[List[Int]]]) = {
+        for (y <- 0 until 9) {
+            for (x <- 0 until 9) {
+                if (completeSudokuGrid(x)(y).size == 1) {
+                    print(completeSudokuGrid(x)(y)(0) + " ")
+                } else {
+                    print("_ ")
+                }
+            }
+            println()
         }
-      }
-      println()
-    }
-  }
-  
-  def solveSudoku(sudokuArray: Array[Array[List[Int]]]) = {
-    def arrayFiltersList (allSingle: Array[Int], i: Int) = {
-      if (allSingle.exists(_ == i)) false else true
-    }
-//    first thing to do is to make a list of all the single lists
-//    in each x position (columns) and then remove this numbers from the lists with
-//    more than one int.  
-    def solveVertical(y: Int ): Boolean = {
-      if (y < 9) {
-        val solvedCellsArray = for (x <- sudokuArray if x(y).length == 1) yield x(y)(0)
-        for (x <- sudokuArray) if (x(y).length > 1) {
-          x(y) = x(y).toList filter (i => arrayFiltersList(solvedCellsArray, i))
-        } 
-        solveVertical(y + 1)
-      }
-      true
-    }
-//    now do (nearly) the same thing horizontally 
-    def solveHorizontal(x: Int ): Boolean = {
-      if (x < 9) {
-        val solvedCellsArray = for (y <- (0 to 8) if sudokuArray(x)(y).length == 1) yield sudokuArray(x)(y)(0)
-        for (y <- (0 to 8) if sudokuArray(x)(y).length > 1) {
-          sudokuArray(x)(y) = sudokuArray(x)(y).toList filter (i => arrayFiltersList(solvedCellsArray.toArray, i))
-        } 
-        solveHorizontal(x + 1)
-      }    	
-      
-      true
     }
 
-    solveVertical(0)
-    solveHorizontal(0)
-    
-//    def pos (y: Int, x: Int) = {
-//      sudokuArray(y)(x)
-//    }
-//
-//    def horizontalAnswers (y: Int) = {
-//      (for (x <- (0 to 8) if pos(y,x).length == 1) yield pos(y,x)(0)).toArray
-//    }
-//
-//    def verticalAnswers (x: Int) = {
-//      (for (y <- (0 to 8) if pos(y,x).length == 1) yield pos(y,x)(0)).toArray
-//    }
-    
-//    def calcH(y: Int) = {
-//      sudokuArray(y).indexWhere()
-//      sudokuArray(y)
-//    }
-    
-//    def diagonal(yx: Int): Array[Array[List[Int]]] = {
-//      if (yx < sudokuArray.length) {
-//        sudokuArray(yx) = sudokuArray(yx) foreach (l => l filter (i => case ) )
-//        diagonal(yx + 1)
-//      }
-//      sudokuArray
-//
-//    }
-    
-    def test(x: Array[List[Int]]) = {
-      println(x)
-      true
-    }
-    
-//    println(sudokuArray.indexWhere { (x) => test(x) })
-//    println(sudokuArray.view.zipWithIndex foreach {case (value, index) => })
-//    diagonal(0)
-    sudokuArray
-  }
-  
-  def genSudokuGrid (list: List[Char]) = {
-    def formattedGrid = {
-      list.map(ch => ch match {
-        case '_' => (1 to 9).toList 
-        case _ => List(ch.toInt - 48)
-      }) 
-    }
-    formattedGrid.toArray grouped 9 toArray
-  }
+    def solveSudoku(sudokuArray: Array[Array[List[Int]]]) = {
 
-  def loadFile = {
-    val chooser = new JFileChooser()
-    val view = chooser.getFileSystemView()
-//    TODO: re-enable file selection
-//    val in = Console.readLine("Place the Sudoku text file in the '" + view.getHomeDirectory + "' directory. \nEnter its filename here (minus the 'txt' suffix): ")
-//    use file.exist to check if it exists
-    
-    fromFile(view.getHomeDirectory + "/" + "sudoku" + ".txt")
-  }
-  
-  def formatFile (file: scala.io.BufferedSource) = {
-   
-    def fileAsString = {
-      file.getLines mkString "\n"
-    }
-    
-    def filterString = {
-      fileAsString filter ("_123456789" contains _ )
-    } 
-    
-    def fileListToArray = {
-      filterString.toList
-    }   
-        
-    fileListToArray 
-  }
-  
+        // used by the solve* functions to determine if the current location has only one value
+        def arrayFiltersList(allSingle: Array[Int], i: Int) = {
+            if (allSingle.exists(_ == i)) false else true
+        }
 
+        // first thing to do is to make a list of all the single lists
+        // in each x position (columns) and then remove this numbers from the lists with
+        // more than one int.
+        def solveVertical(y: Int): Boolean = {
+            if (y < 9) {
+                val solvedCellsArray = for (x <- sudokuArray if x(y).length == 1) yield x(y)(0)
+                for (x <- sudokuArray) if (x(y).length > 1) {
+                    x(y) = x(y).toList filter (i => arrayFiltersList(solvedCellsArray, i))
+                }
+                solveVertical(y + 1)
+            }
+            true
+        }
+
+        // now do (nearly) the same thing horizontally (rows)
+        def solveHorizontal(x: Int): Boolean = {
+            if (x < 9) {
+                val solvedCellsArray = for (y <- 0 to 8 if sudokuArray(x)(y).length == 1) yield sudokuArray(x)(y)(0)
+                for (y <- 0 to 8 if sudokuArray(x)(y).length > 1) {
+                    sudokuArray(x)(y) = sudokuArray(x)(y).toList filter (i => arrayFiltersList(solvedCellsArray.toArray, i))
+                }
+                solveHorizontal(x + 1)
+            }
+            true
+        }
+
+        // now do (nearly) the same thing for boxes
+        def solveBoxes(x: Int, y:Int): Boolean = {
+            if (x < 9 && y < 9) {
+                val solvedCellsArray = for (yy <- y-(y%3) to y+2-(y%3); xx <- x-(x%3) to x+2-(x%3) if sudokuArray(xx)(yy).length == 1) yield sudokuArray(xx)(yy)(0)
+                for (yy <- y-(y%3) to y+2-(y%3) ; xx <- x-(x%3) to x+2-(x%3) if sudokuArray(xx)(yy).length > 1) {
+                    sudokuArray(xx)(yy) = sudokuArray(xx)(yy).toList filter (i => arrayFiltersList(solvedCellsArray.toArray, i))
+                }
+                solveBoxes(if(x==8) 0 else x + 1, if(x==8) y+1 else y)
+            }
+            true
+        }
+
+        solveVertical(0)
+        solveHorizontal(0)
+        solveBoxes(7,7)
+
+        sudokuArray
+    }
+
+    // ask the user to enter the file to load
+    def loadFile = {
+        val chooser = new JFileChooser()
+        val view = chooser.getFileSystemView
+        val in = Console.readLine("Place the Sudoku text file in the '" + view.getHomeDirectory + "' directory. \nEnter its filename here (minus the 'txt' suffix): ")
+        fromFile(view.getHomeDirectory + "/" + in + ".txt")
+    }
+
+    // alter the file so it can be more easily rocessed by the program
+    def formatFile(file: scala.io.BufferedSource) = {
+
+        def fileAsString = {
+            file.getLines mkString "\n"
+        }
+
+        def filterString = {
+            fileAsString filter ("_123456789" contains _)
+        }
+
+        def fileListToArray = {
+            filterString.toList
+        }
+
+        fileListToArray
+    }
+
+    // create a 9 by 9 array from the formatted sudoku file entered by the user
+    def genSudokuGrid(list: List[Char]) = {
+        def formattedGrid = {
+            list.map {
+                case '_' => (1 to 9).toList
+                case ch => List(ch.toInt - 48)
+            }
+        }
+        (formattedGrid.toArray grouped 9).toArray
+    }
 }
